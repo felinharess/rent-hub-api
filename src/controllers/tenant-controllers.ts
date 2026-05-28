4342d99197e98291c445c42a7ad6de7727411f77
felinharess <felinharesdomingues@gmail.com>
2026-05-28 15:49:18 -0300

    feat: fixed tenants's controller

--- src/controllers/tenant-controllers.ts
+++ src/controllers/tenant-controllers.ts
@@ -1,14 +1,7 @@
 import { Request, Response } from 'express';
 import * as TenantServices from '../services/tenant-services';
-import * as httpHelper from '../utils/http-helper'
-export const getTenant = async (req: Request, res: Response) => {
-    try {
-        const httpResponse = await TenantServices.getTenantService();
-        return res.status(httpResponse.statusCode).json(httpResponse.body);
 
-    } catch (error) {
-        console.log("Error: ", error)
-        let response = await httpHelper.internalServerError(error);
-        return res.status(response.statusCode).json(response.body)
-    }
+export const getTenant = async (req: Request, res: Response) => {
+    const httpResponse = await TenantServices.getTenantService();
+    return res.status(httpResponse.statusCode).json(httpResponse.body);
 };
--- src/services/tenant-services.ts
+++ src/services/tenant-services.ts
@@ -1,7 +1,13 @@
 import * as TenantsRepository from '../repositories/tenants-repository';
 import * as httpResponse from '../utils/http-helper';
 
-export const getTenantService = async() =>{
-    const data = await TenantsRepository.findAllTenants();
-    return await httpResponse.ok(data);
+export const getTenantService = async () => {
+    try {
+        const data = await TenantsRepository.findAllTenants();
+        return await httpResponse.ok(data);
+    }catch(error){
+        console.log("Internal Server error:", error);
+        return await httpResponse.internalServerError(error);
+    };
+
 };
